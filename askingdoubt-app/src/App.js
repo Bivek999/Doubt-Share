bce5b50c92a1906261646f2be9d9831cc0d80f88
Bivek999 <118502596+Bivek999@users.noreply.github.com>
2023-12-29 22:01:43 +0530

    firs

--- askingdoubt-app/src/App.js
+++ askingdoubt-app/src/App.js
@@ -1,21 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Login";
-import { Route, Router, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Register from "./Register";
-
+const router=createBrowserRouter([{path:"/", element:<Login/>},{path:"/Register", element:<Register/>}])
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/Register" element={<Register />}></Route>
-        </Routes>
-      </Router>
-      <Login />
-    </div>
-  );
+  return <RouterProvider router={router} />
+    
+  
 }
 
 export default App;
--- askingdoubt-app/src/index.js
+++ askingdoubt-app/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContextProvider } from './Context';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App/>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
